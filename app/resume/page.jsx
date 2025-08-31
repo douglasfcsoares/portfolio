491bed46eb752892aedcb20ba4f6cc37ddee564a
douglasfcsoares <douglasfcsoares@gmail.com>
2025-08-31 15:17:08 -0300

    init resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,44 @@
+"use client"
+
+import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs} from 'react-icons/fa'
+import {SiTailwindcss, SiNextdotjs} from 'react-icons/si'
+
+// about data
+const about = {
+  title: 'About me',
+  description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, quae ducimus. Eos odio rem nemo voluptates quam facere. Distinctio beatae soluta ut esse ad deserunt architecto facere magnam aliquid fuga.',
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: 'Douglas Soares'
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: '(+55) 48 9 9139-5727'
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: '1+ Years'
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: 'Brasil'
+    },
+    {
+      fieldName: "Email",
+      fieldValue: 'douglasfcsoares@gmail.com'
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: 'Available'
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: 'Português'
+    },
+  ]
+}
+
 
 const Resume = () => {
   return (
