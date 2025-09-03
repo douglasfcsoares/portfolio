5e16863583325710ae9d1e48a4dc511f28302122
douglasfcsoares <douglasfcsoares@gmail.com>
2025-09-02 23:03:26 -0300

    init work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,62 @@
-const Work = () => {
-  return (
-    <div>work page</div>
-  )
-}
+"use client";
 
-export default Work
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolore quis inventore facilis explicabo mollitia minus, harum itaque ut, debitis atque sed in tempore voluptatum impedit nostrum! Minus, a adipisci.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "javascript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: ""
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "project 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolore quis inventore facilis explicabo mollitia minus, harum itaque ut, debitis atque sed in tempore voluptatum impedit nostrum! Minus, a adipisci.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: ""
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "project 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolore quis inventore facilis explicabo mollitia minus, harum itaque ut, debitis atque sed in tempore voluptatum impedit nostrum! Minus, a adipisci.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: ""
+  }
+];
+
+const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+  return <motion.div>work page</motion.div>;
+};
+
+export default Work;
